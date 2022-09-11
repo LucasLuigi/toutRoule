483c3becfae38dec4b0693197e7cdc63d44e4b60
LucasLuigi <lukilucas@hotmail.fr>
2022-09-11 04:03:33 +0200

    Killed global variables: becoming a better person Added attempts in the first search of close stations

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ import requests
 import json
 from secret import orsSecretKey
 
-JCDStaticData = []
-JCDStaticDataReduced = []
 
-
-def parseStaticJCDData():
-    global JCDStaticData
-
+def parseStaticJCDData(JCDStaticData):
     try:
         for station in JCDStaticData:
             station["coordinates"] = str(
                 station["latitude"])+","+str(station["longitude"])
     except Exception as err:
-        print(f"[X] Parsing - Static data are not completing formatted: {err}")
+        print(f"[X] Static data are not correctly formatted: {err}")
 
 
-def reduceNumberOfStations(addrFrom):
-    global JCDStaticData
-    global JCDStaticDataReduced
+def reduceNumberOfStations(addrFrom, JCDStaticData):
+    # Measured in Toulouse downtown
+    # Latitude: 0.01° = 1.12km
+    # Longitude: 0.01° = 805m
+    # Mean ~ 1km
+    KM_TO_DEGREES_CONVERTION = 0.01
+    SQUARE_FIRST_HALF_LENGTH_DEGREES = 0.0025
+    SQUARE_INCREMENTED_LENGTH_DEGREES = 0.0025
+    MINIMAL_NB_REDUCED_STATIONS = 2
+    # If we are more than 2km away from any station: we give up
+    MAX_NB_OF_KMS = 2.0
+    MAX_NB_OF_ATTEMPTS = 1 + int(
+        MAX_NB_OF_KMS * KM_TO_DEGREES_CONVERTION / SQUARE_INCREMENTED_LENGTH_DEGREES)
+    JCDStaticDataReduced = []
 
     addrFromSplitted = addrFrom.split(',')
     try:
@@ -29,21 +35,32 @@ def reduceNumberOfStations(addrFrom):
         lon = float(addrFromSplitted[1].strip(" "))
     except Exception as err:
         print(
-            f"[X] Reducing - Static data are not completing formatted: {err}")
+            f"[X] Static data are not correctly formatted: {err}")
 
-    # FIXME find the correct trade-off between "less than 40 stations" (for the API) and "not 0 anywhere"
-    # Maybe we can go very little and extend GAP_DEGREES
+    nbStationFound = 0
+    squareHalfLengthDegrees = SQUARE_FIRST_HALF_LENGTH_DEGREES
+    nbAttempts = 0
 
-    # Measured in Toulouse downtown
-    # Latitude: 0.01° = 1.12km
-    # Longitude: 0.01° = 805m
-    GAP_DEGREES = 0.0025
-    for station in JCDStaticData:
-        if abs(station["latitude"]-lat) <= GAP_DEGREES and abs(station["longitude"]-lon) <= GAP_DEGREES:
-            JCDStaticDataReduced.append(station)
+    # Find closed stations in a square arround the coordinates. If not enough are found, extend the size of the square
+    while nbStationFound == 0:
+        nbAttempts += 1
+        for station in JCDStaticData:
+            if abs(station["latitude"]-lat) <= squareHalfLengthDegrees and abs(station["longitude"]-lon) <= squareHalfLengthDegrees:
+                JCDStaticDataReduced.append(station)
+                nbStationFound += 1
 
-    print(len(JCDStaticData))
-    print(len(JCDStaticDataReduced))
+        if nbStationFound < MINIMAL_NB_REDUCED_STATIONS:
+            # Not enough results
+            squareHalfLengthDegrees += SQUARE_INCREMENTED_LENGTH_DEGREES
+            # Make sure the next count is exact
+            nbStationFound = 0
+
+        if nbAttempts > MAX_NB_OF_ATTEMPTS:
+            raise ValueError(
+                f"[X] The address is too far away (more than {MAX_NB_OF_KMS} kms) than any station.")
+
+    #print(f"DEBUG: {nbAttempts} attempts, found {nbStationFound} stations")
+    return JCDStaticDataReduced
 
 
 # API: https://nominatim.org/release-docs/latest/api/Search/
@@ -51,7 +68,7 @@ def getCoordsFromAddr(addr):
     addrSplitted = addr.strip(' ').split(',')
     try:
         if len(addrSplitted) != 2:
-            raise Exception(f"Length is {addrSplitted}")
+            raise IndexError(f"Length is {addrSplitted}")
         else:
             lat = float(addrSplitted[0].strip(" "))
             lon = float(addrSplitted[1].strip(" "))
@@ -109,28 +126,40 @@ def getDistORS(addrFrom, addrTo):
 
 
 def main():
-    global JCDStaticData
+    JCDStaticData = []
+    JCDStaticDataReduced = []
 
     # stub, while the retrieving of JCDedaux's static data is not implement
     with open('toulouse.json') as toulouseStaticFile:
         JCDStaticData = json.load(toulouseStaticFile)
-    parseStaticJCDData()
+    parseStaticJCDData(JCDStaticData)
 
     #addrFrom = '1 rue Valade, Toulouse'
     addrFrom = '43.60798370654071, 1.4415337673273596'
     addrFromCoord = getCoordsFromAddr(addrFrom)
 
-    reduceNumberOfStations(addrFromCoord)
+    try:
+        JCDStaticDataReduced = reduceNumberOfStations(
+            addrFromCoord, JCDStaticData)
 
-    # TEST, to remove
-    addrFrom = '43.610949082290205, 1.4684277881390817'
-    addrFromCoord = getCoordsFromAddr(addrFrom)
-    reduceNumberOfStations(addrFromCoord)
-    # END OF TEST
+        # TEST, to remove
+        # addrFrom = '43.65723761277264, 1.2737243831651757'
+        # addrFromCoord = getCoordsFromAddr(addrFrom)
+        # #tmp1 = reduceNumberOfStations(addrFromCoord, JCDStaticData)
 
-    # stub. Later, it will be the station
+        # addrFrom = '43.610949082290205, 1.4684277881390817'
+        # addrFromCoord = getCoordsFromAddr(addrFrom)
+        # tmp2 = reduceNumberOfStations(addrFromCoord, JCDStaticData)
+        # END OF TEST
+
+    except ValueError as err:
+        print(err)
+        exit(1)
+
+    # STUB. Later, it will be the station
     addrTo = '6 Rue Antoine Deville, Toulouse'
     addrToCoord = getCoordsFromAddr(addrTo)
+    # END OF STUB
 
     distance, duration = getDistORS(addrFromCoord, addrToCoord)
     print(f"[-] Distance={distance}m, Duration={duration}s")
@@ -140,3 +169,4 @@ if __name__ == '__main__':
     print('- toutRoule - \n')
 
     main()
+    exit(0)
