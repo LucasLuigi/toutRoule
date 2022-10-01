ac9cb14425228b8f5e10fa502c99de0dff4abf79
LucasLuigi <lukilucas@hotmail.fr>
2022-10-01 03:30:09 +0200

    Added secret.py (with no secret of course)

---
+++ secret.py
@@ -0,0 +1,7 @@
+# You need to register for free and ask a free API key for openroute service: https://openrouteservice.org/dev/#/home
+# It is used to access direction computing API
+orsSecretKey = "XXXXXXXXXXXXXXXX"
+
+# You need to register for free and ask a free API key for JCDecaux developer: https://developer.jcdecaux.com/#/account
+# It is used to access JCDecaux bike stations in Toulouse (other cities are available)
+jcdSecretKey = "XXXXXXXXXXXXXXXX"
